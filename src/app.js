d519e69cb3c53fc33ea54913255138cb08686586
anajbsouza <anaju.batsouza@gmail.com>
2023-06-26 16:15:20 -0300

    get implementado, precisa verificar se está mandando os ultimos 10 tweets

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 
-const PORT = 5000;
+const PORT = 5001;
 
 const app = express(); 
 app.use(cors());
@@ -19,18 +19,35 @@ app.post('/sign-up', (req, res) => {
         res.status(201).send("Ok");
     }
     users.push({ username, avatar});
-    console.log(users);
+    //console.log(users);
 });
 
 app.post('/tweets', (req, res) => {
     const { username, tweet } = req.body;
     const userExists = users.find(user => user.username === username);
 
-    if (!userExists)
+    if (!userExists) 
         return res.send('UNAUTHORIZED');
 
     tweets.push({ username, tweet });
     res.status(200).send('Ok');
-})
+});
+
+app.get('/tweets', (req, res) => {
+    const completeInfo = tweets.map((tweet) => {
+        const user = users.find((u) => u.username === tweet.username);
+        if (user) {
+            return {
+                username: tweet.username,
+                tweet: tweet.tweet,
+                avatar: user.avatar
+            }
+        }
+    });
+    if (completeInfo.length > 10) {
+        return completeInfo.slice(-10);
+    }
+    res.send(completeInfo);
+});
 
 app.listen(PORT, () => console.log(`Rodando servidor na porta ${PORT}`));
